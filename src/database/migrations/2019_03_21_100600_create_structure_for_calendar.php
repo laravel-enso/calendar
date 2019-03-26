f4f36f2f484dbde6358ba6f831a64b76d43377d2
Adrian Ocneanu <aocneanu@gmail.com>
2019-03-26 10:33:43 +0000

    Apply fixes from StyleCI

--- src/database/migrations/2019_03_21_100600_create_structure_for_calendar.php
+++ src/database/migrations/2019_03_21_100600_create_structure_for_calendar.php
@@ -5,6 +5,6 @@ use LaravelEnso\StructureManager\app\Classes\StructureMigration;
 class CreateStructureForCalendar extends StructureMigration
 {
     protected $menu = [
-        'name' => 'Calendar', 'icon' => 'calendar-alt', 'route' => 'core.calendar.events.index', 'order_index' => 200, 'has_children' => false
+        'name' => 'Calendar', 'icon' => 'calendar-alt', 'route' => 'core.calendar.events.index', 'order_index' => 200, 'has_children' => false,
     ];
 }
