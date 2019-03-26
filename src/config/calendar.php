4f9c54e3794745a45af14ec6a2f1bf3c96c82499
mishu <mishu@xtelecom.ro>
2019-03-26 13:18:49 +0200

    removes extra calendar

--- src/config/calendar.php
+++ src/config/calendar.php
@@ -3,10 +3,8 @@
 return [
     'calendars' => [
         1 => 'Personal',
-        2 => 'Financial',
     ],
     'calendarClasses' => [
         1 => 'is-primary',
-        2 => 'is-danger',
     ],
 ];
