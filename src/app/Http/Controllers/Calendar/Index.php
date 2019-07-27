d027d168fefcee1e85397e78dfac6cc9edbc434e
reza <raftx24@gmail.com>
2019-07-28 02:21:25 +0430

    fix styleci

--- src/app/Http/Controllers/Calendar/Index.php
+++ src/app/Http/Controllers/Calendar/Index.php
@@ -4,7 +4,6 @@ namespace LaravelEnso\Calendar\app\Http\Controllers\Calendar;
 
 use Illuminate\Routing\Controller;
 use LaravelEnso\Calendar\app\Enums\Calendars;
-use LaravelEnso\DataImport\app\Enums\ImportTypes;
 
 class Index extends Controller
 {
