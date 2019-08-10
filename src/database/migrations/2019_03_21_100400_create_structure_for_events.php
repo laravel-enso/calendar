74254cfbbba65b297c46c45c612d448a62a9c98d
mishu <mishu@xtelecom.ro>
2019-08-10 13:40:01 +0300

    update events structure

--- src/database/migrations/2019_03_21_100400_create_structure_for_events.php
+++ src/database/migrations/2019_03_21_100400_create_structure_for_events.php
@@ -5,8 +5,6 @@ use LaravelEnso\Migrator\app\Database\Migration;
 class CreateStructureForEvents extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.calendar.index', 'description' => 'Show Calendars', 'type' => 0, 'is_default' => true],
-        ['name' => 'core.calendar.events.index', 'description' => 'Show index for event', 'type' => 0, 'is_default' => true],
         ['name' => 'core.calendar.events.create', 'description' => 'Create a new event', 'type' => 0, 'is_default' => true],
         ['name' => 'core.calendar.events.store', 'description' => 'Store a new event', 'type' => 1, 'is_default' => true],
         ['name' => 'core.calendar.events.edit', 'description' => 'Edit event', 'type' => 0, 'is_default' => true],
