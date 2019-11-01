407ef1aadf4401fb3cd82f56e46e94cf16a706f6
Mihai <mihai.ocneanu@gmail.com>
2019-11-01 12:00:06 +0200

    renamed pivot migration

--- src/database/migrations/2019_03_21_100200_create_calendar_event_user_pivot_table.php
+++ src/database/migrations/2019_03_21_100200_create_calendar_event_user_pivot_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEventUserPivotTable extends Migration
+class CreateCalendarEventUserPivotTable extends Migration
 {
     public function up()
     {
