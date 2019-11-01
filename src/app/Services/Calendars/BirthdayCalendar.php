e6c99ea57a965cf073634c7109075e0de7069f44
Mihai <mihai.ocneanu@gmail.com>
2019-11-01 15:25:14 +0200

    refactored query logic for the birthday calendar

--- src/app/Services/Calendars/BirthdayCalendar.php
+++ src/app/Services/Calendars/BirthdayCalendar.php
@@ -10,6 +10,9 @@ use LaravelEnso\People\app\Models\Person;
 
 class BirthdayCalendar implements CustomCalendar
 {
+    private $startDate;
+    private $endDate;
+
     public function getKey()
     {
         return 'birthday-calendar';
@@ -37,15 +40,55 @@ class BirthdayCalendar implements CustomCalendar
 
     public function events(Carbon $startDate, Carbon $endDate): Collection
     {
-        $year = $endDate->format('Y');
-        $start = $startDate->format('Y-m-d');
-        $end = $endDate->format('Y-m-d');
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
 
         return Person::query()
-            ->whereRaw("STR_TO_DATE(DATE_FORMAT(birthday, '$year-%m-%d'), '%Y-%m-%d') BETWEEN '$start' AND '$end' ")
+            ->when($this->withinSameYear(), $this->sameYearQuery())
+            ->when(!$this->withinSameYear(), $this->differentYearQuery())
             ->get()
-            ->map(function ($person) use ($endDate) {
-                return new PersonBirthdayEvent($person, $endDate->year);
+            ->map(function ($person) {
+                return new PersonBirthdayEvent($person, $this->year($person));
             });
+    }
+
+    private function withinSameYear(): bool
+    {
+        return $this->endDate->month >= $this->startDate->month;
+    }
+
+    private function sameYearQuery()
+    {
+        return function ($query) {
+            $query
+                ->whereMonth('birthday', '>=', $this->startDate)
+                ->whereDay('birthday', '>=', $this->startDate)
+                ->whereMonth('birthday', '<=', $this->endDate)
+                ->whereDay('birthday', '<=', $this->endDate);
+        };
+    }
+
+    private function differentYearQuery()
+    {
+        return function ($query) {
+            $query->where(function ($query) {
+                $query->whereMonth('birthday', $this->startDate)
+                    ->whereDay('birthday', '>=', $this->startDate);
+            })->orWhere(function ($query) {
+                $query->whereMonth('birthday', '>', $this->startDate);
+            })->orWhere(function ($query) {
+                $query->whereMonth('birthday', $this->endDate)
+                    ->whereDay('birthday', '<=', $this->endDate);
+            })->orWhere(function ($query) {
+                $query->whereMonth('birthday', '<', $this->endDate);
+            });
+        };
+    }
+
+    private function year($person)
+    {
+        return $person->birthday->month === $this->startDate->month
+            ? $this->startDate->year
+            : $this->endDate->year;
     }
 }
