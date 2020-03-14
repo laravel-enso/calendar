ed0e71bb7af9fac191468ffc2b7076e1aa8cd79e
mishu <mishu@xtelecom.ro>
2020-03-14 18:05:36 +0200

    removes Types from migrations

--- src/database/migrations/2019_03_21_100400_create_structure_for_events.php
+++ src/database/migrations/2019_03_21_100400_create_structure_for_events.php
@@ -1,17 +1,16 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForEvents extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.calendar.events.index', 'description' => 'Get events', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.calendar.events.create', 'description' => 'Create a new event', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.calendar.events.store', 'description' => 'Store a new event', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.calendar.events.edit', 'description' => 'Edit event', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.calendar.events.update', 'description' => 'Update event', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.calendar.events.destroy', 'description' => 'Delete event', 'type' => Types::Write, 'is_default' => true],
+        ['name' => 'core.calendar.events.index', 'description' => 'Get events', 'is_default' => true],
+        ['name' => 'core.calendar.events.create', 'description' => 'Create a new event', 'is_default' => true],
+        ['name' => 'core.calendar.events.store', 'description' => 'Store a new event', 'is_default' => true],
+        ['name' => 'core.calendar.events.edit', 'description' => 'Edit event', 'is_default' => true],
+        ['name' => 'core.calendar.events.update', 'description' => 'Update event', 'is_default' => true],
+        ['name' => 'core.calendar.events.destroy', 'description' => 'Delete event', 'is_default' => true],
     ];
 
     protected $parentMenu = '';
--- src/database/migrations/2019_03_21_100600_create_structure_for_calendar.php
+++ src/database/migrations/2019_03_21_100600_create_structure_for_calendar.php
@@ -1,18 +1,17 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForCalendar extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.calendar.create', 'description' => 'Create a new calendar', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.calendar.store', 'description' => 'Store a new calendar', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.calendar.edit', 'description' => 'Edit calendar', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.calendar.update', 'description' => 'Update calendar', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.calendar.destroy', 'description' => 'Delete calendar', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.calendar.index', 'description' => 'Get calendars', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.calendar.options', 'description' => 'Get options for select', 'type' => Types::Read, 'is_default' => true],
+        ['name' => 'core.calendar.create', 'description' => 'Create a new calendar', 'is_default' => true],
+        ['name' => 'core.calendar.store', 'description' => 'Store a new calendar', 'is_default' => true],
+        ['name' => 'core.calendar.edit', 'description' => 'Edit calendar', 'is_default' => true],
+        ['name' => 'core.calendar.update', 'description' => 'Update calendar', 'is_default' => true],
+        ['name' => 'core.calendar.destroy', 'description' => 'Delete calendar', 'is_default' => true],
+        ['name' => 'core.calendar.index', 'description' => 'Get calendars', 'is_default' => true],
+        ['name' => 'core.calendar.options', 'description' => 'Get options for select', 'is_default' => true],
     ];
 
     protected $menu = [
