72f965e16b900db37ce63b62c7d5ccad3cb88944
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 15:37:46 +0300

    reordered imports

--- routes/app/calendar.php
+++ routes/app/calendar.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use LaravelEnso\Calendar\Http\Controllers\Calendar\Index;
 use LaravelEnso\Calendar\Http\Controllers\Calendar\Create;
-use LaravelEnso\Calendar\Http\Controllers\Calendar\Store;
-use LaravelEnso\Calendar\Http\Controllers\Calendar\Edit;
-use LaravelEnso\Calendar\Http\Controllers\Calendar\Update;
 use LaravelEnso\Calendar\Http\Controllers\Calendar\Destroy;
+use LaravelEnso\Calendar\Http\Controllers\Calendar\Edit;
+use LaravelEnso\Calendar\Http\Controllers\Calendar\Index;
 use LaravelEnso\Calendar\Http\Controllers\Calendar\Options;
+use LaravelEnso\Calendar\Http\Controllers\Calendar\Store;
+use LaravelEnso\Calendar\Http\Controllers\Calendar\Update;
 
 Route::get('', Index::class)->name('index');
 Route::get('create', Create::class)->name('create');
