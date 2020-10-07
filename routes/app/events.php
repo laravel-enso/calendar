ae81e0b5cf03ba90e676a104ff932b1a21dfb0c7
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 15:35:12 +0300

    updates syntax

--- routes/app/events.php
+++ routes/app/events.php
@@ -1,15 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Calendar\Http\Controllers\Events\Create;
+use LaravelEnso\Calendar\Http\Controllers\Events\Destroy;
+use LaravelEnso\Calendar\Http\Controllers\Events\Edit;
+use LaravelEnso\Calendar\Http\Controllers\Events\Index;
+use LaravelEnso\Calendar\Http\Controllers\Events\Store;
+use LaravelEnso\Calendar\Http\Controllers\Events\Update;
 
-Route::namespace('Events')
-    ->prefix('events')
+Route::prefix('events')
     ->as('events.')
     ->group(function () {
-        Route::get('', 'Index')->name('index');
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('{event}/edit', 'Edit')->name('edit');
-        Route::patch('{event}', 'Update')->name('update');
-        Route::delete('{event}', 'Destroy')->name('destroy');
+        Route::get('', Index::class)->name('index');
+        Route::get('create', Create::class)->name('create');
+        Route::post('', Store::class)->name('store');
+        Route::get('{event}/edit', Edit::class)->name('edit');
+        Route::patch('{event}', Update::class)->name('update');
+        Route::delete('{event}', Destroy::class)->name('destroy');
     });
