3da21d1d602618e81c77b1425c93a859dfbc49d7
Abdullahi Abdul Kabir <abdullahiabdulkabir1@gmail.com>
2024-01-17 07:21:13 +0100

    upgrade dependencies

--- src/Dynamics/Relations/Calendars.php
+++ src/Dynamics/Relations/Calendars.php
@@ -3,11 +3,17 @@
 namespace LaravelEnso\Calendar\Dynamics\Relations;
 
 use Closure;
+use LaravelEnso\DynamicMethods\Contracts\Relation;
 use LaravelEnso\Calendar\Models\Calendar;
-use LaravelEnso\DynamicMethods\Contracts\Method;
+use LaravelEnso\Users\Models\User;
 
-class Calendars implements Method
+class Calendars implements Relation
 {
+    public function bindTo(): array
+    {
+        return [User::class];
+    }
+
     public function name(): string
     {
         return 'calendars';
@@ -15,7 +21,7 @@ class Calendars implements Method
 
     public function closure(): Closure
     {
-        return fn () => $this
+        return fn(User $user) => $user
             ->hasMany(Calendar::class, 'created_by');
     }
 }
